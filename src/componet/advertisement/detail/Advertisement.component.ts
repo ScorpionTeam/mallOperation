cd85d73ca99395565761ddee76e2b18b057eac96
HWS <15757135983@163.com>
2017-11-23 09:32:24 +0800

    1.广告模块优化

--- src/componet/advertisement/detail/Advertisement.component.ts
+++ src/componet/advertisement/detail/Advertisement.component.ts
@@ -16,6 +16,7 @@ export class AdvertisementComponent implements OnInit{
   banner:any={
     status:true
   };
+  initPicUrl:any;
   validateForm:FormGroup;
   constructor(private fb:FormBuilder,private http:Http,private nzMessage:NzMessageService,
                 private router:Router,private route:ActivatedRoute,private dataTool:DataTool){}
@@ -29,7 +30,6 @@ export class AdvertisementComponent implements OnInit{
   crateValidate(){
     this.validateForm = this.fb.group({
       name:["",[Validators.required]],
-      imageUrl:["",[Validators.required]],
       actionUrl:["",[Validators.required]],
       sort:["",[Validators.required]],
       status:""
@@ -41,14 +41,15 @@ export class AdvertisementComponent implements OnInit{
    * @param val
    */
   uploadSuccess(val){
-    this.banner.imgList = val[0].url;
+    this.banner.imgUrl = val[0].url;
+    console.log(this.banner.imgUrl);
   }
 
   /**
    * 图片删除成功
    */
   delSuccess(){
-    this.banner.imgList = '';
+    this.banner.imgUrl = '';
   }
 
   /**
@@ -59,6 +60,7 @@ export class AdvertisementComponent implements OnInit{
       this.nzMessage.warning("请上传图片");
       return;
     }else if(!this.validateForm.valid){
+      console.log(this.validateForm);
       this.nzMessage.warning("请将表单填写完整");
       return;
     }
@@ -78,6 +80,7 @@ export class AdvertisementComponent implements OnInit{
         if(res["result"]==1){
           this.nzMessage.success("新增活动成功");
           this.validateForm.reset();
+          this.initPicUrl='';
         }
       },
       err=>{
@@ -102,4 +105,16 @@ export class AdvertisementComponent implements OnInit{
       }
     )
   }
+
+  /**
+   * 返回
+   */
+  back(){
+    if((this.route.params['value'].id)){
+      this.router.navigate(["../../banner-list"],{relativeTo:this.route});
+    }else {
+      this.router.navigate(["../banner-list"],{relativeTo:this.route});
+    }
+  }
+
 }
